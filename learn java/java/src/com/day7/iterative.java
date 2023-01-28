965442a5b985fc12f4757c52aed79299859ef401
Yogesh saini <101331023+yogeshsaini21@users.noreply.github.com>
2023-01-28 16:28:41 +0530

    tutorial 7 notes uploaded

---
+++ learn java/java/src/com/day7/iterative.java
@@ -0,0 +1,25 @@
+package com.day7;
+
+public class iterative {
+		public static void main(String[] args) {
+			
+//			int count=0;
+//			int i=0;
+//			while(i<=10) {
+//				System.out.println(i+"condition true");
+//				count++;
+//				i=i+2;
+//			}
+//			System.out.println("total number of iterations "+count); 
+			
+			int i=0;
+			do
+			{
+			System.out.println(i);
+			i=i+1;
+			}
+			while (i<0);
+			}
+			
+		
+}
